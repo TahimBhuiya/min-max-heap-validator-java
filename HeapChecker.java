c513458f16d6243ef62e69b1671d032a50646851
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-07-25 15:59:15 -0400

    Finished isMaxHeap function

--- HeapChecker.java
+++ HeapChecker.java
@@ -11,3 +11,10 @@ public class HeapChecker {
     }
 
     public static boolean isMaxHeap(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i <= (n - 2) / 2; i++) {
+            if (2 * i + 1 < n && arr[i] < arr[2 * i + 1]) return false;
+            if (2 * i + 2 < n && arr[i] < arr[2 * i + 2]) return false;
+        }
+        return true;
+    }
